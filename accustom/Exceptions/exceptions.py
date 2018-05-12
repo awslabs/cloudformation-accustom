c421391a8b2d8ff6c238c9802d603c050da874be
nightkhaos <nightkhaos+gpg@gmail.com>
2018-05-12 14:49:57 +1000

    Formatting improvements

--- accustom/Exceptions/exceptions.py
+++ accustom/Exceptions/exceptions.py
@@ -2,32 +2,39 @@
 
 These are the exceptions that can be returned by accustom
 """
+
+
 class CannotApplyRuleToStandaloneRedactionConfig(Exception):
     """Indicates that a second rule set was attempted to be applied to a standalone"""
-    def __init__(self,*args,**kwargs):
-        Exception.__init__(self,*args,**kwargs)
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
 
 class ConflictingValue(Exception):
     """Indicates that there is already a record with this value"""
-    def __init__(self,*args,**kwargs):
-        Exception.__init__(self,*args,**kwargs)
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
 
 class NoPhysicalResourceIdException(Exception):
     """Indicates that there was no valid value to use for PhysicalResourceId"""
-    def __init__(self,*args,**kwargs):
-        Exception.__init__(self,*args,**kwargs)
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
 
 class InvalidResponseStatusException(Exception):
     """Indicates that there response code was not SUCCESS or FAILED"""
-    def __init__(self,*args,**kwargs):
-        Exception.__init__(self,*args,**kwargs)
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
 
 class DataIsNotDictException(Exception):
     """Indicates that a Dictionary was not passed as Data"""
-    def __init__(self,*args,**kwargs):
-        Exception.__init__(self,*args,**kwargs)
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
 
 class FailedToSendResponseException(Exception):
     """Indicates there was a problem sending the response"""
-    def __init__(self,*args,**kwargs):
-        Exception.__init__(self,*args,**kwargs)
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
